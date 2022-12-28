2cac42b43e009fba5bed146194127fc27e384404
Young JIn <leesu0229@naver.com>
2022-12-28 13:07:45 +0900

    JS: arrow-up btn, 홈 투명

--- main.js
+++ main.js
@@ -14,15 +14,51 @@ document.addEventListener('scroll', () => {
 
 
 // Handle scrolling when tapping the navbar menu
-document.addEventListener('click', (event) => {
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
 
     if(link == null) {
         return;
     }
-    console.log(event.target.dataset.link);
-
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollTo(link);
 });
+
+// Home contact button
+
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', (event) => {
+    scrollTo('#contact');
+});
+
+function scrollTo(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+
+// Make home transparnet when scrolling
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Hnadling arrow-up button
+
+const arrow = document.querySelector('.arrow');
+
+document.addEventListener('scroll', () => {
+    if(homeHeight < window.scrollY) {
+        arrow.classList.add('visible');
+    } else {
+        arrow.classList.remove('visible');
+    }
+});
+
+arrow.addEventListener('click', () => {
+    scrollTo('#home');
+});
+
