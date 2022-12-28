80922e80a48762837c56582daddf140d86c9fa2d
Young JIn <leesu0229@naver.com>
2022-12-28 15:27:04 +0900

    JS: project filering

--- main.js
+++ main.js
@@ -62,3 +62,27 @@ arrow.addEventListener('click', () => {
     scrollTo('#home');
 });
 
+
+// Project Filter
+
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (event) => {
+    const filter = event.target.dataset.filter ||
+     event.target.parentNode.dataset.filter;
+
+     if(filter == null) {
+        return;
+     } 
+    projects.forEach((project) => {
+        if(filter === '*' || filter=== project.dataset.type) {
+            project.classList.remove('invisible');
+        } else {
+            project.classList.add('invisible');
+        }
+    });
+    
+    console.log(filter);
+});
